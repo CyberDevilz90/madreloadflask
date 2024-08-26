d84f7258d4132af6e42e4197207f5af674006b1e
cyberdevilz90 <ahmadmahmudi2000@gmail.com>
2024-08-26 22:52:47 +0700

    Route to create order SMM

--- apps/transactions/routes.py
+++ apps/transactions/routes.py
@@ -1,8 +1,12 @@
 from flask import Blueprint
-from apps.transactions.functions import perform_transaction_ppob
+from apps.transactions.functions import perform_transaction_ppob, perform_transaction_sosmed
 
 transactions = Blueprint('transactions', __name__)
 
 @transactions.route('/create-order', methods=['POST'])
-def handle_perform_transaction():
+def handle_perform_transaction_ppob():
     return perform_transaction_ppob()
+
+@transactions.route('/create-order-ssm', methods=['POST'])
+def handle_perform_transaction_ssm():
+    return perform_transaction_sosmed()
