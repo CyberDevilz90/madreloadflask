f12f162e2d4e1fe5b84c62ef0a8d8bbcb5921425
cyberdevilz90 <ahmadmahmudi2000@gmail.com>
2024-08-27 09:53:43 +0700

    crons job for update smm transaction

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask, request
+from sqlalchemy import or_
 from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import datetime, timedelta
-from apps.models import TransactionPPOB, db, User
+from apps.models import TransactionPPOB, TransactionSocialMedia, db, User
 import requests
 from apps import create_app
 import hashlib
@@ -12,6 +13,10 @@ DIGIFLAZZ_API_URL = 'https://api.digiflazz.com/v1/transaction'
 DIGIFLAZZ_API_KEY = 'a8beff67-cb39-5be2-b4cf-afe22f7e0bab'
 # DIGIFLAZZ_API_KEY = "dev-9790e880-5ce5-11ec-af18-b53e1be9e9ea"
 DIGIFLAZZ_USERNAME = 'biduguopZ9GW'
+
+BUZZERPANEL_API_URL = 'https://buzzerpanel.id/api/json.php'
+BUZZERPANEL_API_KEY = 'kl1fvb5pa4z9te3082su7qrxjcmd6o'
+BUZZERPANEL_SECRET_KEY = 'uYJQ4cMAanFijWO17egthwNGp53HVkBx0PfRS8Kmo9lE2dIrvD'
 
 def generate_sign(username, api_key, ref_id):
     sign_str = f"{username}{api_key}{ref_id}"
@@ -76,10 +81,73 @@ def update_pending_transactions():
                 
             except requests.exceptions.RequestException as e:
                 print(f"Error updating transaction {transaction.id}: {e}")
+                
+def update_pending_smm():
+    with app.app_context():
+        pending_smm = TransactionSocialMedia.query.filter(
+            or_(
+                TransactionSocialMedia.status == 'Pending',
+                TransactionSocialMedia.status == 'pending',
+                TransactionSocialMedia.status == 'In progress',
+                TransactionSocialMedia.status == 'Processing'
+            )
+        ).all()
+        
+        if not pending_smm:
+            print(f"No pending transaction found at {datetime.now()}")
+            return
+    
+        for transaction in pending_smm:
+            payload = {
+                "action": 'status',
+                "api_key": BUZZERPANEL_API_KEY,
+                "secret_key": BUZZERPANEL_SECRET_KEY,
+                "id": transaction.id_pesanan
+            }
+
+            try:
+                response = requests.post(BUZZERPANEL_API_URL, data=payload)
+                response.raise_for_status()
+
+                data = response.json()
+                print(f"Response : {data}")
+
+                if 'data' in data:
+                    transaction_status = data['data'].get('status')
+                    start_count = data['data'].get('start_count')
+                    remains = data['data'].get('remains')
+
+                    if transaction_status == 'Processing':
+                        transaction.status = transaction_status
+                        transaction.start_count = start_count
+                        transaction.remains = remains
+                    elif transaction_status == 'In progress':
+                        transaction.status = transaction_status
+                        transaction.start_count = start_count
+                        transaction.remains = remains
+                    elif transaction_status == 'Partial':
+                        transaction.status = transaction_status
+                        transaction.start_count = start_count
+                        transaction.remains = remains
+
+                        user = User.query.get(transaction.user_id)
+                        if user:
+                            sisa = transaction.price/1000 * remains
+                            user.balance += sisa
+
+                    db.session.commit()
+
+                    print(f"Updated transaction {transaction.id} to status {transaction.status} at {datetime.now()}")
+
+            except requests.exceptions.RequestException as e:
+                print(f"Error updating transaction {transaction.id} : {e}")
+            except Exception as e:
+                print(f"Unexpected error for transaction {transaction.id}: {e}")
 
 if __name__ == "__main__":
     scheduler = BackgroundScheduler()
     scheduler.add_job(update_pending_transactions, 'interval', minutes=1)
+    scheduler.add_job(update_pending_smm, 'interval', minutes=1)
     scheduler.start()
     
     try:
